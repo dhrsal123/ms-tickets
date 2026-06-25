cbecee2c2f3736fbad5adec8fe17671b9f914b15
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-25 13:47:31 -0500

    feat: add base files

---
+++ src/main/java/io/cinema/mstickets/MsTicketsApplication.java
@@ -0,0 +1,13 @@
+package io.cinema.mstickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/cinema/mstickets/MsTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cinema.mstickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
