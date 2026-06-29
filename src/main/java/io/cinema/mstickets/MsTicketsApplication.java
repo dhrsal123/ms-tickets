7152ca7451370a687b2646e2fe54916e22988a95
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-29 12:18:04 -0500

    feat: add auditing configs

--- src/main/java/io/cinema/mstickets/MsTicketsApplication.java
+++ src/main/java/io/cinema/mstickets/MsTicketsApplication.java
@@ -1,9 +1,14 @@
 package io.cinema.mstickets;
 
+import io.cinema.config.AuditingConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "io.cinema")
+@Import({AuditingConfig.class})
 public class MsTicketsApplication {
 
     public static void main(String[] args) {
